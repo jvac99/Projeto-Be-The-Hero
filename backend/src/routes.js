1efd8ffbe7d6b59fa960d8ba11c2da3f6f6b089d
João Victor <48702646+jvac99@users.noreply.github.com>
2021-11-08 23:03:47 -0300

    Backend-update

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,14 +7,11 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions',SessionController.create);
-
-routes.get('/ongs',OngController.index);
+routes.post('/sessions', SessionController.create);
+routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
-
 routes.get('/profile', ProfileController.index);
-
-routes.get('/incidents',IncidentController.index);
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
